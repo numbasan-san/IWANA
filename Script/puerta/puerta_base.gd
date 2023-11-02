bff8f342cffa126f29c144e8c89dad51f7156381
Julkson <julkson@gmail.com>
2023-11-02 00:39:06 -0300

    Agregados varios objetos
    - Cambiado nombre y ubicación de puerta_simple a puerta_base.
    - puerta_base ahora es la base de todas las puertas, por lo que no tiene un sprite por defecto
    - Hay unos cambios en el script puerta_base que hacen que no funcione en este commit, pero si en uno futuro
    - Agregados objetos para los baños
    - Agregadas casi todas las puertas
    - Agregados lockers

--- Script/puerta/puerta_base.gd
+++ Script/puerta/puerta_base.gd
@@ -2,10 +2,11 @@
 extends Area2D
 
 @export var zona : String
-@export var posicion_def: Vector2
+@export var spawn: String
 @export var direccion_def: String
 
 func change_zone(personaje: Personaje):
 	var mundo = $/root/Juego/ControladorPantallas.pantallaMundo.get_node("Mundo")
 	var zona_objetivo = $/root/Juego.cargar_zona(zona)
-	mundo.recolocar_personaje(personaje, zona_objetivo, posicion_def, direccion_def)
+	
+	mundo.spawn(personaje, zona_objetivo, spawn, direccion_def)
