24ec0dcf33e2230d548cbce28003600c8c454d4d
Julkson <julkson@gmail.com>
2023-11-04 16:22:42 -0300

    Modificaciones a puertas
    - Creada una escena puerta_invisible, que se usará para las puertas que se colocan en la muralla
    	sur de las zonas y que no tienen modelos
    - Se cambiaron los tamaños de la zona de detección de puerta tanto en las puertas en sí como
    	en el jugador, para que tenga que acercarse más a las puertas para gatillar el cambio de zona
    - Se agregó un fondo oscuro que sigue a la camara y se dibuja en un z-layer bajo para que siempre
    	se dibuje antes que todo. Esto sirve como un fondo para que no se vea el color por defecto, y
    	se puede cambiar por otro color o textura que se vea mejor más adelante
    - Se separaron las areas de detección de colisión con items y con puertas en dos nodos distintos
    - Se separó la función _on_interacción en dos funciones, una para los items y una para las puertas
    - Se cambiaron los puntos de Spawn para que el jugador aparezca más cerca de las puertas, pero sin
    	entrar en contacto con ellas

--- Script/Personajes/control_jugador.gd
+++ Script/Personajes/control_jugador.gd
@@ -13,8 +13,8 @@ func _process(_delta):
 	var y = int(Input.is_action_pressed('rpg_down')) - int(Input.is_action_pressed('rpg_up'))
 	get_parent().set_axis(x, y)
 	
-# Las colisiones con objetos cuya colisión tenga una función o evento en especial.
-func _on_interaccion(area):
+# Las colisiones con items en el suelo.
+func _on_interaccion_item(area):
 	if area.has_method('collect'): # La colisión del avatar del jugador con un objeto.
 		if inventory.count_empty_slot() >= 1: # En caso de que haya espacio en el inventario.
 			area.collect(inventory)
@@ -22,14 +22,20 @@ func _on_interaccion(area):
 			# Se verifica si en el último espacio del inventario hay espacio en el stack del ítem en turno.
 			if inventory.count_stacks():
 				area.collect(inventory)
-	# La colisión del avatar del jugador con una "puerta".
+
+
+# Entrar al area de interaccion de una puerta
+func _on_interaccion_puerta(area):
 	if area.has_method('change_zone'):
 		area.change_zone(get_parent().personaje)
 
+
+# Entrar al area de interaccion de un objeto con transparencia
 func _on_entrar_transparencia(area):
 	if area is ControlTransparencia and area.global_position.y > get_parent().global_position.y:
 		area.activar_transparencia()
-		
+
+# Salir del area de interaccion de un objeto con transparencia
 func _on_salir_transparencia(area):
 	if area is ControlTransparencia:
 		area.desactivar_transparencia()
