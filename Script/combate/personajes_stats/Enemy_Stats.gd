c221b2d4e9b6ae7b278b44b4ce0bfccbf1f80aa6
numbasan-san <80220352+numbasan-san@users.noreply.github.com>
2023-11-06 01:27:22 -0400

    Add. Prototipo de Combate.
    Creación del prototipo del sistema de combate.

---
+++ Script/combate/personajes_stats/Enemy_Stats.gd
@@ -0,0 +1,7 @@
+
+extends Resource
+
+@export var name : String
+@export var texture : Texture
+@export var health : int
+@export var damage : int
---
+++ Script/combate/personajes_stats/Player.gd
@@ -0,0 +1,8 @@
+
+extends Node
+
+class_name Player_Stats
+
+var current_health = 35
+var max_health = 35
+var damage = 20
