282d687301329cb00d08eda6b3b1f645c40d37e5
Julkson <julkson@gmail.com>
2023-11-10 17:05:34 -0300

    Arreglados errores en integración con combate
    - Botones de pantalla de combate funcionan nuevamente
    - Animación In y Out se muestran correctamente
    - Combate usa pop para volver a la pantalla anterior
    - Falta resetear el estado del combate cuando finaliza para
    	evitar que se llame a pop repetidamente

--- Script/combate/combate.gd
+++ Script/combate/combate.gd
@@ -53,7 +53,7 @@ func _process(delta):
 	if end_game:  # Cierre del combate.
 		await(textbox_closed)
 		await(get_tree().create_timer(0.5))
-		get_tree().quit()
+		$/root/Juego.pantallas.pop()
 		
 
 # Se establece la vida según qué barra de vida.
@@ -89,7 +89,7 @@ func _on_run_pressed():
 	display_text('Como buen cobarde, huiste.')
 	await(textbox_closed)
 	await(get_tree().create_timer(0.5))
-	get_tree().quit()
+	$/root/Juego.pantallas.pop()
 
 # El ataque del jugador.
 func _on_attack_pressed():
