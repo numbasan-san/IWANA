dfc5d2c4c76eb57c9cd73e6ca990e74b6f3a6db3
Julkson <julkson@gmail.com>
2023-11-14 12:50:57 -0300

    Arreglo de bugs y pantallas
    - Finalmente arreglado bug que desconecta nodos de pantalla de combate
    - Cambiado layout de pantalla de combate para que se vea bien con la nueva
    	resolución
    - Cambiado layout del modo dev para que se vea bien con la nueva resolución

--- script/dev_mode.gd
+++ script/dev_mode.gd
@@ -32,6 +32,7 @@ func rellenar_lista_zonas():
 		boton.text = zona.name
 		boton.focus_mode = Control.FOCUS_NONE
 		boton.mouse_filter = Control.MOUSE_FILTER_STOP
+		boton.add_theme_font_size_override("font_size", 30)
 		boton.pressed.connect(func () -> void:
 			var jugador = $"/root/Juego".personajes.jugador
 			mundo.spawn(jugador, zona, "Default", "down", mundo.SpawnFallback.FIRST)
