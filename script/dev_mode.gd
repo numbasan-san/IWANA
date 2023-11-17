619198bd85b739c9035ddc7b91e5549f2c129715
Julkson <julkson@gmail.com>
2023-11-16 21:38:04 -0300

    Cambios en dev mode
    - Ahora los botones de cambio de zona se convirtieron en un menú popup que
    	aparece al hacer click en el nombre de la zona actual
    - Se agregaron botones que muestran la escena y unidad actual. Al hacer click
    	sobre ellos se puede cargar una escena desde el principio, o seleccionar
    	una unidad de la escena actual para ejecutar desde allí
    - Se agregó código para mostrar qué linea de diálogo de la unidad actual se
    	está mostrando
    - Cambios varios al layout de la pantalla
    - Se movió el arreglo con las zonas del juego desde la instancia de la raíz
    	a la escena dev_mode
    - Se desactiva la selección de zona si el jugador no se encuentra ya en el juego
    	para evitar algunos errores

--- script/dev_mode.gd
+++ script/dev_mode.gd
@@ -1,18 +1,37 @@
 extends Pantalla
 
 @export var todas_las_zonas: Array[String]
+@export var zonas: MenuButton
+@export var escenas: MenuButton
+@export var unidades: MenuButton
 
 @onready var mundo = $/root/Juego.pantallas.pantalla_mundo.get_node("Mundo")
+@onready var controlador_guion: ControladorGuion = $/root/Juego/ControladorGuion
 
 var habilitado = false
 
 func _process(_delta):
 	if habilitado:
 		if $"/root/Juego".personajes.jugador and $"/root/Juego".personajes.jugador.zona:
-			$Contenidos/DetallesUbicacion/NombreZona.text = $"/root/Juego".personajes.jugador.zona.name
+			zonas.disabled = false
+			zonas.text = $"/root/Juego".personajes.jugador.zona.name
 		else:
-			$Contenidos/DetallesUbicacion/NombreZona.text = ""
+			zonas.disabled = true
+			zonas.text = "Ninguna"
+		var escena_actual = controlador_guion.escena_actual
+		if escena_actual:
+			escenas.text = escena_actual.nombre
+			if escena_actual.unidad_actual:
+				unidades.text = escena_actual.unidad_actual.nombre
+			else:
+				unidades.text = "Ninguna"
+		else:
+			escenas.text = "Ninguna"
+			unidades.text = "Ninguna"
+			
 	if $"/root/Juego".pantallas.pantalla_dialogo.visible:
+		var linea = controlador_guion.escena_actual.unidad_actual.linea_actual
+		$Contenidos/DetallesDialogo/Linea.text = str(linea)
 		$Contenidos/DetallesDialogo.show()
 	else:
 		$Contenidos/DetallesDialogo.hide()
@@ -22,19 +41,82 @@ func habilitar():
 	activar()
 	habilitado = true
 	rellenar_lista_zonas()
+	rellenar_lista_escenas()
+	# Esta función solo busca las unidades de la escena actual, así que siempre
+	# es seguro llamarla
+	rellenar_lista_unidades()
 
 func rellenar_lista_zonas():
 	if not habilitado:
 		return
+	var popup: PopupMenu = zonas.get_popup()
+	popup.clear()
+	popup.add_theme_font_size_override("font_size", 30)
+	# Esta variable es necesaria porque varias funciones del popup necesitan el
+	# indice, pero al agregar un item no entrega el indice asignado, por lo que
+	# hay que asignarle un id manualmente y obtener el indice a traves de el
+	var item_id = 0
 	for nombre_zona in todas_las_zonas:
 		var zona = $"/root/Juego".zonas.cargar(nombre_zona)
-		var boton = Button.new()
-		boton.text = zona.name
-		boton.focus_mode = Control.FOCUS_NONE
-		boton.mouse_filter = Control.MOUSE_FILTER_STOP
-		boton.add_theme_font_size_override("font_size", 30)
-		boton.pressed.connect(func () -> void:
+		popup.add_item(zona.name, item_id)
+		var indice = popup.get_item_index(item_id)
+		item_id += 1
+		popup.set_item_metadata(indice, zona)
+		
+	# Para conectar la señal asumimos que no esta señal nunca va a ser conectada
+	# a otra función fuera de este script. Si en algun momento eso cambia, hay
+	# que arreglar este código
+	if popup.index_pressed.get_connections().size() == 0:
+		popup.index_pressed.connect(func (index) -> void:
+			var zona = popup.get_item_metadata(index)
 			var jugador = $"/root/Juego".personajes.jugador
 			mundo.spawn(jugador, zona, "Default", "down", mundo.SpawnFallback.FIRST)
 		)
-		$Contenidos/SeleccionEscena/PanelContainer/VBoxContainer/Zonas.add_child(boton)
+
+func rellenar_lista_escenas():
+	if not habilitado:
+		return
+	var popup: PopupMenu = escenas.get_popup()
+	popup.clear()
+	popup.add_theme_font_size_override("font_size", 30)
+	# Esta variable es necesaria porque varias funciones del popup necesitan el
+	# indice, pero al agregar un item no entrega el indice asignado, por lo que
+	# hay que asignarle un id manualmente y obtener el indice a traves de el
+	var item_id = 0
+	for nombre_escena in controlador_guion.escenas:
+		popup.add_item(nombre_escena, item_id)
+		item_id += 1
+		
+	# Para conectar la señal asumimos que no esta señal nunca va a ser conectada
+	# a otra función fuera de este script. Si en algun momento eso cambia, hay
+	# que arreglar este código
+	if popup.index_pressed.get_connections().size() == 0:
+		popup.index_pressed.connect(func (index) -> void:
+			var nombre_escena = popup.get_item_text(index)
+			controlador_guion.cargar(nombre_escena)
+			rellenar_lista_unidades()
+		)
+
+func rellenar_lista_unidades():
+	if not habilitado:
+		return
+	var popup: PopupMenu = unidades.get_popup()
+	popup.clear()
+	popup.add_theme_font_size_override("font_size", 30)
+	# Esta variable es necesaria porque varias funciones del popup necesitan el
+	# indice, pero al agregar un item no entrega el indice asignado, por lo que
+	# hay que asignarle un id manualmente y obtener el indice a traves de el
+	var item_id = 0
+	for nombre_unidad in controlador_guion.escena_actual.unidades:
+		popup.add_item(nombre_unidad, item_id)
+		var indice = popup.get_item_index(item_id)
+		item_id += 1
+		
+	# Para conectar la señal asumimos que no esta señal nunca va a ser conectada
+	# a otra función fuera de este script. Si en algun momento eso cambia, hay
+	# que arreglar este código
+	if popup.index_pressed.get_connections().size() == 0:
+		popup.index_pressed.connect(func (index) -> void:
+			var nombre_unidad = popup.get_item_text(index)
+			controlador_guion.escena_actual.cargar(nombre_unidad)
+		)
