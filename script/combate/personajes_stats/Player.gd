57f6a66a995397917d1a26925046c2e782821e14
Julkson <julkson@gmail.com>
2023-11-28 00:07:47 -0300

    Traducción nombres de scripts
    - Traducidos los nombres de los scripts y cambiadas referencias a ellos
    - Eliminados archivos en desuso

--- script/combate/personajes_stats/Player.gd
+++
@@ -1,8 +0,0 @@
-
-extends Node
-
-class_name Player_Stats
-
-var current_health = 35
-var max_health = 35
-var damage = 20
--- script/inventario_objetos/items/objetos/lagarto.gd
+++
@@ -1,9 +0,0 @@
-
-extends "res://script/inventario_objetos/items/collectable.gd"
-
-@onready var anim = $AnimationPlayer
-
-func collect(inventory : Inventory):
-	anim.play("taked")
-	await anim.animation_finished
-	super(inventory)
