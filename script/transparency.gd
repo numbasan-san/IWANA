e93ff3f237b7d8aae191f1ff8746fcf9ead14473
Julkson <julkson@gmail.com>
2024-01-03 22:39:00 -0300

    Fixes to zones and objects sizes and sprites
    - Changed position and offsets of sprites in some characters and objects so that
    	their pivot is at an appropiate position for y sorting
    - Adjusted player colliders
    - Changed sprite files to ones with the correct sizes and that hopefully allow
    	for more orderly sprites
    - Added code to the doors to allow to switch between the front and back sprites
    	and to make them transparent when they should go on the south walls.
    - Replaced "invisible" doors with regular doors with transparency and removed scenes
    	for doors with back sprites
    - WIP of the new combat screen
    - Fixed errors with y sorting between player and world objects
    - Adjusted some colliders for objects

--- script/transparency.gd
+++ script/transparency.gd
@@ -2,10 +2,11 @@ class_name TransparencyControl
 extends Area2D
 
 @export var transparency_value = 0.15
+@export var sprite: Sprite2D
 
 func enable_transparency():
-	$"../Sprite2D".modulate = Color(1, 1, 1, transparency_value)
+	sprite.modulate = Color(1, 1, 1, transparency_value)
 
 
 func disable_transparency():
-	$"../Sprite2D".modulate = Color(1, 1, 1, 1)
+	sprite.modulate = Color(1, 1, 1, 1)
